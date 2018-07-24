21e68901b87f040d81484760465e6fe83594a8f9
Ranar Basnet <basnetranar@gmail.com>
2018-07-24 21:56:10 +1000

    home page completed

--- src/home/home.js
+++ src/home/home.js
@@ -23,6 +23,7 @@ angular.module('myApp.home', ['ngRoute'])
 	$scope.price = 0;
 	$scope.volume = 0;
 	$scope.change = 0;
+	let labels12 = [], data1 = [], data2 = [];
 
 	$scope.selectedCurrencyChanged = function(){
     $scope.Currency = $scope.selectedCurrency;
@@ -38,18 +39,60 @@ angular.module('myApp.home', ['ngRoute'])
 
 		$http.get(' https://api.cryptonator.com/api/full/' + $scope.Crypto + "-" + $scope.Currency)
 		.then(function successCallback(response){
-		$scope.result = response.data;
-		$scope.marketArray = $scope.result.ticker.markets;
-		$scope.base = $scope.result.ticker.base;
-		$scope.price = $scope.result.ticker.price;
-		$scope.volume = $scope.result.ticker.volume;
-		$scope.change = $scope.result.ticker.change;
-		$scope.currentTime = Date();
+			$scope.result = response.data;
+			$scope.marketArray = $scope.result.ticker.markets;
+			$scope.base = $scope.result.ticker.base;
+			$scope.price = $scope.result.ticker.price;
+			$scope.volume = $scope.result.ticker.volume;
+			$scope.change = $scope.result.ticker.change;
+			$scope.currentTime = Date();
+			getChart();
 		}),
 		function errorCallback(response) {
 			console.log("page is invalid")};
+
+		
+
 		
 	}
+	let getChart = function(){
+		// get the labels and data for the bar charts
+		labels12 = [];  data1 = []; data2 = [];
+		let backgroundColors = ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#3d9e9c",
+								"#ff0000","#ff9d00","#cfb2e1","#b89647","#395c24","#dad6a0"];
+		for(let eachElem of $scope.marketArray){
+			labels12.push(eachElem.market);
+			data1.push(eachElem.price);
+			data2.push(eachElem.volume);
+		};
+
+		// Create bar chart using chartjs
+		let newChart = document.getElementById("barChart1").getContext('2d');
+		if($scope.canvas1!=null){$scope.canvas1.destroy(); console.log('instance destroyed');}
+		
+		$scope.canvas1 = new Chart(newChart, {
+		    type: 'bar',
+		    data: {
+		      labels: labels12,
+		      datasets: [
+		        {
+		          label: "Price ($)",
+		          backgroundColor: backgroundColors,
+		          data: data1
+		        }
+		      ]
+		    },
+		    options: {
+		      legend: { display: false },
+		      title: {
+		        display: true,
+		        text: 'Price($) in each market',
+		        fontSize: 16
+		      }
+		    }
+		});
+	};
+
 	getTicker();
 	$interval(getTicker,30000);
 	
